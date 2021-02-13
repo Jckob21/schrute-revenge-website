54928cb2b563494f3a56d457252319bd9bd14e6e
Jckob21 <jckob21@gmail.com>
2021-02-13 16:04:15 +0100

    Created basic index.php with connection to css/style.css
    Files running on xampp server since now.

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<head>
-    
-</head>
+<!doctype html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <title>Schrute Revenge</title>
+        <meta name="description" content="Schrute Revenge game forum">
+        <meta name="author" content="Jakub Woźny">
+
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <?php
+        echo "Hola sinoritas";
+        ?>
+        <p class="test">Testttt</p>
+    </body>
+
+</html>
