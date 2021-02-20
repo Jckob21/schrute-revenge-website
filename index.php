d90fc668ce1207dccacb494b20b1e721dc146f08
Jacob <jckob21@gmail.com>
2021-02-20 19:14:48 +0100

    Tested the curve of a div.

--- index.php
+++ index.php
@@ -8,14 +8,31 @@
         <meta name="description" content="Schrute Revenge game forum">
         <meta name="author" content="Jakub Woźny">
 
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css\\style.css">
     </head>
 
     <body>
-        <?php
-        echo "Hola sinoritas";
-        ?>
-        <p class="test">Testttt</p>
+        <div class="container">
+            <section class="welcome-section">
+                <header id="logo">
+                    <h1 class="heading">Schrute Revenge</h1>
+                </header>
+                
+                <section class="video-section">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/9aYTdzE-ibI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </section>
+            </section>
+
+            <section class="interested-section">
+                Zainteresowany?
+            </section>
+            
+                        <?php echo 'lul'?>
+
+        </div>
+
+
+         
     </body>
 
 </html>
