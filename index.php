7633e75df5be0561e1b9ccd6b564099800598ed5
Jacob <jckob21@gmail.com>
2021-03-03 18:13:27 +0100

    Created connecting to database and adding email to it
    Created connect.php that is used to connect to database. The code sanitizes the email given, validates it, checks if it is already in the database and inserts it.

--- index.php
+++ index.php
@@ -1,18 +1,91 @@
 <!doctype html>
 <?php
     session_start();
-    /*if(isset($_POST['user_email']))
-    {
-        header("Location ")
-    }*/
 
+    /////////////////////////////////// NEWSLETTER SIGNUP /////////////////////
+    if(isset($_POST['user_email']))
+    {
+        // sanitize & validate email
+        $email = $_POST['user_email'];
+        $emailSanitized = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+        if(filter_var($emailSanitized, FILTER_VALIDATE_EMAIL) && $emailSanitized == $email)
+        {
+            //email validate, pass email to database
+            require_once "connect.php";
+
+            //change to MYSQLI_REPORT_OFF
+            mysqli_report(MYSQLI_REPORT_STRICT);
+
+            try{
+                // create connection to database
+                $connection = new mysqli($host, $db_user, $db_password, $db_name);
+                
+                if($connection->connect_errno != 0)
+                {
+                    throw new Exception(mysqli_connect_errno());
+                } else
+                {
+                    //query for records with such email
+                    $sql_query = sprintf("SELECT * FROM newsletter WHERE email='%s'",
+                        mysqli_real_escape_string($connection, $emailSanitized));
+                    
+                    if($result = $connection->query($sql_query))
+                    {
+                        /* Check if there is such a record with email given
+                         * If there is one, print error
+                         * Unless, insert it to database and print success msg
+                        */
+                        if($result->num_rows > 0)
+                        {
+                            $_SESSION['e_user_email'] = "The email is already signed up for newsletter!";
+                        } else
+                        {
+                            //mysqli query to database
+                            $sql_query = sprintf("INSERT INTO newsletter VALUES(null, '%s')",
+                            mysqli_real_escape_string($connection, $emailSanitized));
+
+                            if($result = $connection->query($sql_query))
+                            {
+                                $_SESSION['s_user_email'] = "Success! You have been signed for a newsletter!";
+                                unset($_SESSION['e_user_email']);
+                            } else
+                            {
+                                throw new Exception($connection->error);
+                            }
+                        }
+                        
+                    } else
+                    {
+                        throw new Exception($connection->error);
+                    }
+
+                    
+                }
+
+                $connection->close();
+            } catch (Exception $e)
+            {
+                echo "Data base error, please try again latter.";
+                echo "Developers info: " . $e;
+            }
+        } else
+        {
+            $_SESSION['e_user_email'] = "Your email is not valid! Please provide another one :)";
+        }
+    }
+
+    /////////////////////////////////// TIMER /////////////////////////////////
+    //get date from server
     $server_date = new DateTime();
     $server_date_formatted = $server_date->format('Y-m-d H:i:s');
-    
     $release_date = DateTime::createFromFormat('Y-m-d H:i:s', '2021-4-1 20:00:00');
-
+    
+    //calculate time till release date
     $time_till_release = $server_date->diff($release_date); //this is a DateInterval object!
     $time_till_release_formatted = $time_till_release->format('%d days, %h:%i:%s');
+    
+    //format result
     $days_release = $time_till_release->format('%d days, ');
     $time_release = $time_till_release->format('%h:%i:%s');
 ?>
@@ -69,7 +142,18 @@
                             </label>
                             <input type="submit" value="Wyślij!">
                         </form>
-                        
+                        <?php
+                        if(isset($_SESSION['e_user_email']))
+                        {
+                            echo '<div class="error">' . $_SESSION['e_user_email'] . "</div>";
+                            unset($_SESSION['e_user_email']);
+                        }
+                        if(isset($_SESSION['s_user_email']))
+                        {
+                            echo '<div class="success">' . $_SESSION['s_user_email'] . "</div>";
+                            unset($_SESSION['s_user_email']);
+                        }
+                        ?>
                     </div>
                 </div>
             </section>
